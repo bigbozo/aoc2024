4fbf7bfe9f90fc74a4439a98f94aea415688d08f
Bugi Goertz <burkhard.goertz@shapefruit.de>
2024-12-02 17:19:36 +0100

    Solves day 2

--- src/Year2024/Day02/Solution.php
+++ src/Year2024/Day02/Solution.php
@@ -26,31 +26,55 @@ class Solution implements SolutionInterface
         $save = 0;
         $lines = explode(PHP_EOL, $inputStream);
         $valid = 0;
+        $valid2 = 0;
         foreach ($lines as $l) {
             if (trim($l)) {
+                $validity = 0;
                 $n = explode(' ', $l);
 
-                $s = array_shift($n);
-                $distances = [];
-                foreach ($n as $num) {
-                    $distances[] = $num - $s;
-                    $s = $num;
-                }
-                sort($distances);
-                $low = array_shift($distances);
-                $high = array_pop($distances);
-                if (
-                    (($low > 0 && $high > 0) || ($low < 0 && $high < 0))
-                    && max(abs($high),abs($low)) <= 3) {
+                if ($this->checkValidity($n)) {
                     $valid++;
-                }
+                    $valid2++;
+                } else {
+                    for($i=0;$i<count($n);$i++) {
+                        $n2 = $n;
+                        unset($n2[$i]);
+                        if ($this->checkValidity($n2)) {
+                            $valid2++;
+                            break;
+                        }
+                    }
+                };
             }
         }
 
         return new SolutionResult(
             2,
             new UnitResult("%s reports are safe", [$valid]),
-            new UnitResult('The 2nd answer is %s', [0])
+            new UnitResult('%s reports are safe when using a Problem Dampener', [$valid2])
         );
+    }
+
+    /**
+     * @param array $n
+     * @param int $validity
+     * @return array
+     */
+    private function checkValidity(array $n): bool
+    {
+        $validity = false;
+        $s = array_shift($n);
+        $distances = [];
+        foreach ($n as $num) {
+            $distances[] = $num - $s;
+            $s = $num;
+        }
+        sort($distances);
+        $low = array_shift($distances);
+        $high = array_pop($distances);
+        return
+            (($low > 0 && $high > 0) || ($low < 0 && $high < 0))
+            && max(abs($high), abs($low)) <= 3;
+
     }
 }
