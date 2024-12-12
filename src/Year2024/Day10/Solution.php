12b401bf697626adc8a38f73f5cbe4625b64d12c
Bugi Goertz <bugi@joboland.de>
2024-12-12 20:45:43 +0100

    solves 2024 day 10 part 1

---
+++ src/Year2024/Day10/Solution.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Bizbozo\AdventOfCode\Year2024\Day10;
+
+use Bizbozo\AdventOfCode\Solutions\SolutionInterface;
+use Bizbozo\AdventOfCode\Solutions\SolutionResult;
+use Bizbozo\AdventOfCode\Solutions\UnitResult;
+use Bizbozo\AdventOfCode\Structures\Board;
+use Override;
+
+class Solution implements SolutionInterface
+{
+
+    public function getTitle(): string
+    {
+        return "Hoof It";
+    }
+
+    #[Override] public function solve(string $inputStream, ?string $inputStream2 = null): SolutionResult
+    {
+
+        $board = new Board($inputStream);
+
+        $queue = array_map(
+            fn($cell) => [
+                /* step */
+                0,
+                /* position */
+                $cell['pos'],
+                /* origin */
+                $cell['id']
+            ],
+            $board->find(fn($cell) => $cell['tile'] == '0')
+        );
+
+        while ($queue) {
+
+            [$counter, $currentPosition, $origin] = array_shift($queue);
+
+            //echo sprintf("%s - %s - %s/%s\n", $origin, $counter, ...$currentPosition);
+
+            $neighbours = $board->findNeighbours($currentPosition);
+
+            $nextStep = array_map(
+                function ($cell) use ($counter, $origin, $board) {
+
+                    $board->board[$cell['pos'][1]][$cell['pos'][0]]['visitors'][$origin] = true;
+
+                    return [
+                        $counter + 1,
+                        $cell['pos'],
+                        $origin
+                    ];
+                },
+                array_filter(
+                    $neighbours,
+                    fn($cell) => $cell['tile'] == $counter + 1
+                )
+            );
+
+
+            $queue = array_merge($queue, $nextStep);
+        }
+
+        $trailHeads = $board->find(
+            fn($item) => $item['tile'] == 9
+        );
+        $sum = array_sum(array_map(
+            fn($item) => count($item['visitors']),
+            $trailHeads
+        ));
+
+
+        return new SolutionResult(
+            10,
+            new UnitResult("The 1st answer is %s", [$sum]),
+            new UnitResult('The 2nd answer is %s', [0])
+        );
+    }
+}
--- tests/Benchmark/AdventOfCodeBench2024.php
+++ tests/Benchmark/AdventOfCodeBench2024.php
@@ -88,12 +88,22 @@ class AdventOfCodeBench2024
 
 
     /**
-     * @Revs(1000)
+     * @Revs(5)
      */
     public function benchDay09(): void
     {
         (new \Bizbozo\AdventOfCode\Year2024\Day09\Solution)
             ->solve(file_get_contents($this->getInputFilename(2024, 9)));
+    }
+
+
+    /**
+     * @Revs(1000)
+     */
+    public function benchDay10(): void
+    {
+        (new \Bizbozo\AdventOfCode\Year2024\Day10\Solution)
+            ->solve(file_get_contents($this->getInputFilename(2024, 10)));
     }
 
 }
