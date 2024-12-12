a410e3cdb109aafee55c469852a561332699b5e5
Bugi Goertz <bugi@joboland.de>
2024-12-12 21:00:32 +0100

    Updated bencher to write machine specific Benchmarks
    set MACHINE in .env

--- src/Commands/BenchmarkCommand.php
+++ src/Commands/BenchmarkCommand.php
@@ -9,44 +9,44 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class BenchmarkCommand extends Command
 {
-  protected function configure()
-  {
-    $this->setDescription('Run the benchmarks for the whole codebase')
-      ->setName('benchmark');
-  }
+    protected function configure()
+    {
+        $this->setDescription('Run the benchmarks for the whole codebase')
+            ->setName('benchmark');
+    }
 
-  protected function execute(InputInterface $input, OutputInterface $output): int
-  {
-    $project_dir = __DIR__ . '/../..';
-    $csvFilePath = "$project_dir/docs/benchmark.csv";
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $project_dir = __DIR__ . '/../..';
+        $csvFilePath = "$project_dir/docs/benchmark.csv";
 
-    exec("php $project_dir/vendor/bin/phpbench run tests/Benchmark --report=default --output=csv > $csvFilePath");
+        exec("php $project_dir/vendor/bin/phpbench run tests/Benchmark --report=default --output=csv > $csvFilePath");
 
-    $fh = fopen($csvFilePath, "r");
-    $rows = [];
-    $widths = [];
-    while ($row = fgetcsv($fh, 4096)) {
-      if (!count($widths)) $widths = array_fill(0, count($row), 0);
-      foreach ($row as $col => &$item) {
-        if (is_numeric($item)) $item = number_format(round($item, 2), 2);
-        $item = " $item ";
-        $widths[$col] = max($widths[$col], strlen($item));
-      }
-      $rows[] = $row;
-    };
-    fclose($fh);
-    @unlink($csvFilePath);
-    $rows = array_map(function ($row) use ($widths) {
-      foreach ($row as $col => $item) {
-        $stream[] = str_pad($item, length: $widths[$col], pad_type: STR_PAD_LEFT, pad_string: " ");
-      }
-      return implode("|", $stream);
-    }, $rows);
-    $stream = "    # Benchmark Results" . PHP_EOL . PHP_EOL . implode(PHP_EOL, $rows);
+        $fh = fopen($csvFilePath, "r");
+        $rows = [];
+        $widths = [];
+        while ($row = fgetcsv($fh, 4096)) {
+            if (!count($widths)) $widths = array_fill(0, count($row), 0);
+            foreach ($row as $col => &$item) {
+                if (is_numeric($item)) $item = number_format(round($item, 2), 2);
+                $item = " $item ";
+                $widths[$col] = max($widths[$col], strlen($item));
+            }
+            $rows[] = $row;
+        };
+        fclose($fh);
+        @unlink($csvFilePath);
+        $rows = array_map(function ($row) use ($widths) {
+            foreach ($row as $col => $item) {
+                $stream[] = str_pad($item, length: $widths[$col], pad_type: STR_PAD_LEFT, pad_string: " ");
+            }
+            return implode("|", $stream);
+        }, $rows);
+        $stream = "    # Benchmark Results" . PHP_EOL . PHP_EOL . implode(PHP_EOL, $rows);
 
-    file_put_contents("$project_dir/docs/benchmark.md", str_replace(PHP_EOL, PHP_EOL.'    ',$stream));
+        file_put_contents("$project_dir/docs/benchmark." . $_ENV['MACHINE'] . ".md", str_replace(PHP_EOL, PHP_EOL . '    ', $stream));
 
 
-    return Command::SUCCESS;
-  }
+        return Command::SUCCESS;
+    }
 }
